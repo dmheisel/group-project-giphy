e66b49701340f32890f2b3f4e0539ce1dfcab9a2
Declan Bernardin <dmjbernardin@yahoo.com>
2019-08-29 14:18:26 -0500

    added linked and displying gif on favorites page

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Favorites extends Component {
-    returnToSearch = () => {
-        this.props.history.push('/')
-    }
+
+componentDidMount(){
+    this.getFavorites()
+}
+
+getFavorites = () => {
+    this.props.dispatch({ type: 'FETCH_FAVS'})
+}
+
+returnToSearch = () => {
+    this.props.history.push('/')
+}
     render() {
+        let images = this.props.reduxStore.favoriteReducer.map((object, index) => {
+            return (<div key={index}>  <img src={object.img_link} /> </div>)
+        })
         return (
             <div>
                 <button onClick={this.returnToSearch}>Search</button>
+                {images}
             </div>
         )
     }
