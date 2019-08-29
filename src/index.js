f907c0a3cfc6e44ef4e03051eaa6ba9f5402e54f
dmheisel <d.m.heisel@gmail.com>
2019-08-29 11:08:47 -0500

    imports and provider set up"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios'
+//redux imports
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+//saga imports
+import createSagaMiddleware from 'redux-saga'
+import { takeEvery, put } from 'redux-saga/effects'
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+
+
+
+const store = createStore(
+  combineReducers({}),
+  applyMiddleware(sagaMiddleware, logger)
+)
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
