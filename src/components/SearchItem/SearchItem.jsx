cf35925750e46e2e90c262eaaee614829a0d4168
dmheisel <d.m.heisel@gmail.com>
2019-08-29 15:19:08 -0500

    styling added to search item component

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //material-ui imports
-import {withStyles} from '@material-ui/core/styles'
-import GridListTileBar from '@material-ui/core/GridListTileBar'
-import IconButton from '@material-ui/core/IconButton'
-import StarIcon from '@material-ui/icons/Star'
-import StarBorderIcon from '@material-ui/icons/StarBorder'
+import Fab from '@material-ui/core/Fab';
+import { withStyles } from '@material-ui/core/styles';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 const styles = theme => ({
 	image: {
@@ -15,23 +16,22 @@ const styles = theme => ({
 	}
 });
 class SearchItem extends Component {
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div>
-                <img src={this.props.gifObject.images.fixed_height.url} alt='gif item' />
-                <GridListTileBar actionIcon={
-                    <IconButton>
-                        <StarBorderIcon />
-                    </IconButton>} />
-
-            </div>
-
-
-        )
-    }
-
+	render() {
+		const { classes } = this.props;
+		return (
+			<div>
+				<img
+					src={this.props.gifObject.images.fixed_height.url}
+					alt='gif item'
+				/>
+				<Fab>
+					<IconButton>
+						<StarBorderIcon />
+					</IconButton>
+				</Fab>
+			</div>
+		);
+	}
 }
 
 export default connect()(withStyles(styles)(SearchItem));
